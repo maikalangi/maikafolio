cf95240bd707bac35592bbfeb09a6334e4aacb8a
maikalangi <maikalangi@gmail.com>
2022-08-04 13:38:34 -0700

    fixed styling

--- src/components/section2.js
+++ src/components/section2.js
@@ -5,36 +5,48 @@ const Section2 = (props)=>{
             <div className="projects">
                 <ul className="projectList">
                     <li>
+                        <h4>Book Finder</h4>
                         <div className="projectCard">
-                            <h4>Book Finder</h4>
                             <a href="https://maikalangi.github.io/books-finder/">
-                                <img className="project-pic" src={props.bookfinder} alt="project pic"/>
+                                <img 
+                                    className="project-pic" 
+                                    src={props.bookfinder}
+                                    alt="project pic"
+                                />
                             </a>
-                                <p>
-                                    A simple book search app that finds and displays info on books. Initial foray into web development applying new skills in HTML, CSS, and Javascript to display data retrieved from an external API and database 
-                                </p>
+                            <p>
+                                A simple book search app that finds and displays info on books. Initial foray into web development applying new skills in HTML, CSS, and Javascript to display data retrieved from an external API and database 
+                            </p>
                         </div>
                     </li>
                     <li>
+                        <h4>To-do List</h4>
                         <div className="projectCard">
-                            <h4>Flowfi</h4>
-                            <a href="https://flowfiapp.herokuapp.com/">
-                                <img className="project-pic" src={props.flowfi} alt="project pic"/>
-                            </a>
-                                <p>
-                                    A web application to streamline the process for entering the cryptocurrency marketplace by offering cryptocurrency exchange options and wallets. A journey through Python and Django utilizing their tools to develop a fullstack application and deploy.
-                                </p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="projectCard">
-                            <h4>To-do List</h4>
                             <a href="https://young-reaches-38531.herokuapp.com/todo">
-                                <img className="project-pic" src={props.todo} alt="project pic"/>
+                                <img 
+                                    className="project-pic" 
+                                    src={props.todo} 
+                                    alt="project pic"
+                                />
                             </a>
-                                <p>
-                                    To-do list for users to add, update, complete, and delete actions to do. Further development of learned skills from Book Finder project with new CRUD function understanding and (non-relational) database utilization implemented to store user input
-                                </p>
+                            <p>
+                                To-do list for users to add, update, complete, and delete actions to do. Further development of learned skills from Book Finder project with new CRUD function understanding and (non-relational) database utilization implemented to store user input
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <h4>Flowfi</h4>
+                        <div className="projectCard">
+                            <a href="https://flowfiapp.herokuapp.com/">
+                                <img 
+                                    className="project-pic" 
+                                    src={props.flowfi} 
+                                    alt="project pic"
+                                />
+                            </a>
+                            <p>
+                                A web application to streamline the process for entering the cryptocurrency marketplace by offering cryptocurrency exchange options and wallets. A journey through Python and Django utilizing their tools to develop a fullstack application and deploy.
+                            </p>
                         </div>
                     </li>
                 </ul>
