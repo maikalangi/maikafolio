dc195671903c616b37b5d23ba5202f8d08c3ceaf
maikalangi <maikalangi@gmail.com>
2022-09-10 20:52:52 -0700

    connect icons

--- src/components/section3.js
+++ src/components/section3.js
@@ -1,17 +1,21 @@
-const Section3 = ()=>{
+import { FiLinkedin } from "react-icons/fi"
+import { BsGithub } from "react-icons/bs"
+import {HiDocumentText} from "react-icons/hi"
+
+const Section3 = (props)=>{
     return (
         <div className="section" id="section3">
             <h3>Connect</h3>
             <div className="connect">
                 <ul>
                     <li>
-                        <a href="https://www.linkedin.com/in/maika-langi/">LinkedIn</a>
+                        <a href="https://www.linkedin.com/in/maika-langi/"><FiLinkedin/></a>
                     </li>
                     <li>
-                        <a href="https://github.com/maikalangi">Github</a>
+                        <a href="https://github.com/maikalangi"><BsGithub/></a>
                     </li>
                     <li>
-                        <a href="https://docs.google.com/document/d/1mcUgQNi_N8dgoxgXVcbS6aJO8Kx_fC-IJavjarZiCXs/edit">Resumé</a>
+                        <a href="https://docs.google.com/document/d/1mcUgQNi_N8dgoxgXVcbS6aJO8Kx_fC-IJavjarZiCXs/edit"><HiDocumentText/></a>
                     </li>
                 </ul>
             </div>
